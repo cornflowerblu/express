78f32ffc97a5196a5f2e3f169509aba86e980b28
rurichot <roger@itsovertime.com>
2022-02-06 16:18:12 -0800

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/message', function(req, res, next) {
+  res.send({
+    "message":"success",
+    "question":"what kind of bear is best?"
+  })
+});
+
+module.exports = router;
